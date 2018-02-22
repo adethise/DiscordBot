5d1c93aac3d2efb9ad7c9bd3bc827fd0f98f4a6f
jonathan <jonathan@visiedo.be>
2018-02-22 20:00:24 +0100

    console.log

--- commands/list.js
+++ commands/list.js
@@ -2,11 +2,14 @@ const Command = require('./command')
 const commandList = require('../commandList.json')
 
 module.exports = class List extends Command{
-  static match(message){
-    return message.content.startsWith('!help')
-  }
+    static match(message){
+        return message.content.startsWith('!help')
+    }
 
-  static action(message){
-    message.author.send("Voici la liste des commandes:")
-  }
+    static action(message){
+        var json = JSON.parse(commandList)
+        console.log(json)
+        message.author.send("Voici la liste des commandes:")
+        message.author.send()
+    }
 }

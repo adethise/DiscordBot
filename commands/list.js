58b626c6318da31b1e6039af7e04bd0badf8d010
jonathan <jonathan@visiedo.be>
2018-02-22 20:11:51 +0100

    init

--- commands/list.js
+++ commands/list.js
@@ -3,12 +3,10 @@ const commandList = require('../commandList.json')
 
 module.exports = class List extends Command{
     static match(message){
-        return message.content.startsWith('!help')
+        return message.content.startsWith('!!help')
     }
 
     static action(message){
         message.author.send("Voici la liste des commandes:")
-
-
     }
 }

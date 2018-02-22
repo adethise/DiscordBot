c2e8de8794f81c33efb86df40dffa4cd5f53a862
jonathan <jonathan@visiedo.be>
2018-02-22 20:09:14 +0100

    init

--- commands/list.js
+++ commands/list.js
@@ -7,9 +7,8 @@ module.exports = class List extends Command{
     }
 
     static action(message){
-        var json = JSON.parse(commandList)
-        console.log(json)
         message.author.send("Voici la liste des commandes:")
-        message.author.send()
+
+
     }
 }

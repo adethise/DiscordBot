0f472774b6cb486f98652f65936cc48749897729
Son <Son.tran.be@gmail.com>
2018-02-25 20:24:28 +0100

    Listing completed - need to get the next entry to make a precises search

--- commands/anime.js
+++ commands/anime.js
@@ -1,34 +1,28 @@
 const auth = require('../auth.json')
 const Command = require('./command')
-const parseString = require('xml2js').parseString;
-const request = require('request');
-
+const malScraper = require('mal-scraper')
 module.exports = class anime extends Command{
 
-    static action(message){
+    static async action(message){
+      var args = message.content.split(' ')
+      args.splice(args,1);
+      let name = args.join(' ');
 
-      let USER = auth.MAL_USER;
-      let PASS = auth.MAL_PASS;
-      let rUrl = "https://myanimelist.net/api/anime/search.xml?q=bleach";
-      request(rUrl, {'auth': {'user':auth.MAL_USER, 'pass':auth.MAL_PASS, "sendImmediately":false}},
-        function (error, response, body) {
-          // Do more stuff with 'body' here
-          message.reply(response.statusCode);
-          if(response.statusCode != 200){message.reply(":("); }
-          if (!error && response.statusCode == 200){
-              try{
-                parseString(body,function(err,result){
-                  console.log(result);
-                  console.log( Object.keys(result['anime']['entry']).length );
-                  console.log(result['anime']['entry'][0]);
-                  message.reply(result['anime']['entry'][0]);
-                });
-              }catch(error){
-                  message.reply('ERROR MSH'+error)
-              }
-          }
-        }
-      );
+      var data;
+    try{
+       data = await malScraper.getResultsFromSearch(name);
+       let len = Object.keys(data).length;
+       var msg = '``` \n';
+       for( var i = 0 ; i < len ; i++ ){
+         msg = msg +'['+i+'] '+data[i]['name'] + '\n';
+       }
+       msg = msg + '```';
+      message.reply(msg);
+    }catch(error){
+        console.log(error);
+      }
+
+
 
     }
 }

08de461beacf731e8df095148e3879b666079409
Son <Son.tran.be@gmail.com>
2018-02-25 22:08:22 +0100

    Module Anime Prêt - Requiert une dépendance pour mal-scraper (npm mal-scraper --save)

--- commands/anime.js
+++ commands/anime.js
@@ -1,28 +1,60 @@
 const auth = require('../auth.json')
 const Command = require('./command')
+const Discord = require('discord.js')
 const malScraper = require('mal-scraper')
 module.exports = class anime extends Command{
 
     static async action(message){
+      function resultReader(result){
+        //console.log(result);
+        const embed = new Discord.RichEmbed()
+        .setTitle(result['title'])
+        .setDescription(result['synopsis'])
+        .setThumbnail(result['picture'])
+        .addField('Status',result['status']+' Aired: '+result['aired'])
+        .addField('Episodes',result['episodes'])
+        .addField('Genres',result['genres'].join(' '))
+        .addField('Score',result['score']+' '+result['scoreStats'])
+        .setColor(0x00AE86)
+        message.channel.send({embed});
+      }
       var args = message.content.split(' ')
-      args.splice(args,1);
+      if( args[1] === undefined){
+        message.channel.send('Baaaaakaaaaaa !! Tu as oublié de préciser ta recherche !');
+        return;
+      }
+      args.shift();
       let name = args.join(' ');
-
-      var data;
+      var data,entry;
     try{
        data = await malScraper.getResultsFromSearch(name);
        let len = Object.keys(data).length;
-       var msg = '``` \n';
+       var msg = 'Chomosuke a trouvé plusieurs animes ! Entres le numéro correspondant à celui que tu cherches ! \n```';
        for( var i = 0 ; i < len ; i++ ){
          msg = msg +'['+i+'] '+data[i]['name'] + '\n';
        }
        msg = msg + '```';
-      message.reply(msg);
+      message.channel.send(msg);
+      const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+        collector.on('collect', async function(message){
+            entry = parseInt(message.content);
+            if( !Number.isInteger(entry) || entry < 0 || entry > 9  ){
+              message.channel.send('Niiiiaaaaarrrkkkk ! Chomosuke ne te comprends pas ! Mets juste le chiffre correspondant.');
+              return;
+            }
+            try{
+              //console.log(data[entry]['name']);
+            const result = await malScraper.getInfoFromName(data[entry]['name']);
+            resultReader(result);
+          }catch(error){
+            console.log(error);
+            return;
+          }
+        })
     }catch(error){
         console.log(error);
-      }
-
-
-
+        return;
     }
+    }
+
 }
